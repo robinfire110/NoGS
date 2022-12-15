7ca1cc5603ff250ea59d911314ce70c656343e88
Andy Villasmil <aevillas@uncg.edu>
2022-12-15 16:08:57 -0500

    Update 1.1

--- js/background.js
+++ js/background.js
@@ -1,52 +1,70 @@
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.local.get(["blocked", "enabled"], function (local) {
-      if (!Array.isArray(local.blocked)) {
-        chrome.storage.local.set({ blocked: [] });
-      }
-  
-      if (typeof local.enabled !== "boolean") {
-        chrome.storage.local.set({ enabled: false });
-      }
+    chrome.storage.local.get(["blocked", "enabled", "ipData", "scope"], function (local) {
+      //Set Array
+      if (!Array.isArray(local.blocked)) chrome.storage.local.set({ blocked: [] });
+
+      //Set enabled
+      if (typeof local.enabled !== "boolean") chrome.storage.local.set({ enabled: false });
+
+      //Set ipData
+      if (local.ipData != undefined) chrome.storage.local.set( { ipData: undefined });
+
+      //Set scope
+      if (local.scope != undefined) chrome.storage.local.set( { scope: undefined });
+
     });
 });
   
 //Check before you navigate to the page
-chrome.webNavigation['onBeforeNavigate'].addListener(function(data) {
+chrome.webNavigation['onBeforeNavigate'].addListener(async function(data) {
   const url = data.url;
   if (!url || !url.startsWith("http")) {
     return;
   }
-  
-  const hostname = new URL(url).hostname;
 
-  chrome.storage.local.get(["blocked", "enabled"], async function (local) {
-    const { blocked, enabled } = local;
+  console.log("Not Blocked", data)
+
+  const hostname = new URL(url).hostname;
+  chrome.storage.local.get(["blocked", "enabled", "ipData", "scope"], async function (local) {
+    const { blocked, enabled, ipData, scope } = local;
     
     //If it is in the list
-    if (Array.isArray(blocked) && enabled && (blocked.find(domain => domain.includes(hostname)) || blocked.find(domain => hostname.includes(domain)))) {
-      if (await checkIp())
+    if (Array.isArray(blocked) && enabled && data.frameId == 0 && await (blocked.find(domain => domain.includes(hostname)) || blocked.find(domain => hostname.includes(domain)))) {
+      if (await checkIp(ipData, scope))
       {
-        chrome.tabs.update(data.tabId, { url: 'chrome-extension://gglceijpcfilfeeobcdogbcfgafpmeoo/blocked.html'})
-        console.log("Blocked", data.url);
+        chrome.tabs.update(data.tabId, { url: 'chrome-extension://gglceijpcfilfeeobcdogbcfgafpmeoo/html/blocked.html'})
+        console.log("Blocked", data);
       }
     }
   });
 });
 
-async function checkIp()
+async function checkIp(ipData, scope)
 {
   //Get IP Address
   var res = await fetch('https://api.ipify.org?format=json');
   var data = await res.json();
-  let ip = data.ip;
+  let ip = await data.ip;
+
+  //Stop here if it's just an ip check (just to make it a bit more effecient)
+  if (scope = "5") return ip != ipData.query;
 
   //Get organization
-  var res = await fetch(`http://ip-api.com/json/${ip}?fields=status,message,country,countryCode,city,isp,org,as,asname,query`);
+  var res = await fetch(`http://ip-api.com/json/${ip}?fields=status,message,countryCode,region,city,zip,org,as,asname,query`);
   var data = await res.json();
-  let check = data.asname;
+  let check = await data;
 
-  //Check
-  //console.log(check);
-  return check == "UNC-GREENSBORO";
+  //Check for scope
+  var result;
+  switch (scope)
+  {
+    case "0": result = check.countryCode != ipData.countryCode; break; //Country
+    case "1": result = check.region != ipData.region; break; //Region
+    case "2": result = check.city != ipData.city; break; //City
+    case "3": result = check.zip != ipData.zip; break; //Zip
+    case "4": result = check.asname != ipData.asname; break; //Organization
+    case "5": result = check.query != ipData.query; break; //IP Address
+  }
+  return await result;
 }
   
--- js/options.js
+++ js/options.js
@@ -1,18 +1,39 @@
 //Get Elements
 const textarea = document.getElementById("textarea");
-const save = document.getElementById("save");
 const checkbox = document.getElementById("checkbox");
+const ipForm = document.getElementById("ipForm");
+const inputIp = document.getElementById("inputIp");
+const useCurrentIp = document.getElementById("useCurrentIp");
+const inputScope = document.getElementById("inputScope");
+const saveButton = document.getElementById("saveButton");
+let currentIp;
+
+//Use current ip button
+useCurrentIp.addEventListener("click", async () => {
+  inputIp.value = await currentIp;
+});
 
 //Set block list
-save.addEventListener("click", () => {
+saveButton.addEventListener("click", async () => {
   //Blocked list
   const blocked = textarea.value.split("\n").map(s => s.trim()).filter(Boolean);
   chrome.storage.local.set({ blocked });
 
-  //Have the user input the remote ip (advise ip chicken or such)
-  //Have user have the ability to select which attribute should be checked (IP Address, Country, Region, City, Zip, Organization)
-  //Write a page that you can go to that explains what each of these do and why you should use one over the other.
+  //Get ipInfo
+  console.log(inputIp.value);
+  var res = await fetch(`http://ip-api.com/json/${inputIp.value}?fields=status,message,countryCode,region,city,zip,org,as,asname,query`);
+  var data = await res.json();
+  const ipData = data;
+  console.log("ipData: ", ipData);
+
+  //Get Selected Scope
+  const scope = inputScope.value;
+
+  //Set
+  chrome.storage.local.set( {ipData} );
+  chrome.storage.local.set( {scope} );
 });
+
 
 //Set Enabled
 checkbox.addEventListener("change", (event) => {
@@ -21,12 +42,36 @@ checkbox.addEventListener("change", (event) => {
 });
 
 //Refill With Data when load
-window.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.local.get(["blocked", "enabled"], function (local) {
-    const { blocked, enabled } = local;
+window.addEventListener("DOMContentLoaded", async () => {
+  chrome.storage.local.get(["blocked", "enabled", "ipData", "scope"], function (local) {
+    const { blocked, enabled, ipData, scope} = local;
+
+    //Add to textarea
     if (Array.isArray(blocked)) {
       textarea.value = blocked.join("\n");
-      checkbox.checked = enabled;
     }
+
+    //Check enabled
+    checkbox.checked = enabled;
+
+    //Set IP
+    console.log("ran");
+    if (ipData == undefined) inputIp.value = currentIp; //If there is nothing saved, get and use current ip
+    else inputIp.value = ipData.query; //Fill the one from the data
+
+    //Set Scope
+    if (scope != undefined) inputScope.value = scope;
   });
+
+  //Get Current Ip
+  currentIp = await getCurrentIp();
 });
+
+//Get Current Ip
+async function getCurrentIp()
+{
+  //Get Current IP (to use in various places)
+  var res = await fetch('https://api.ipify.org?format=json');
+  var data = await res.json();
+  return data.ip;
+}
